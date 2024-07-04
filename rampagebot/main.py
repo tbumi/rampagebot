63ebe7aafcdeb57b4a1073e2724b7b142430beb0
Trapsilo Bumi <git@tbumi.id>
2024-07-04 14:27:33 +0100

    Implement statistics endpoint

--- rampagebot/main.py
+++ rampagebot/main.py
@@ -1,6 +1,9 @@
 from contextlib import asynccontextmanager
+from datetime import datetime
+from pathlib import Path
+from typing import Annotated
 
-from fastapi import FastAPI, status
+from fastapi import Body, FastAPI, status
 
 from rampagebot.IdleBot import IdleBot
 from rampagebot.models.Commands import Command
@@ -20,6 +23,7 @@ async def lifespan(app: FastAPI):
         TeamName.RADIANT: SmartBot(TeamName.RADIANT),
         TeamName.DIRE: IdleBot(TeamName.DIRE),
     }
+    app.state.started_time = datetime.now()
     app.state.games_remaining = NUMBER_OF_GAMES
     yield
 
@@ -58,16 +62,57 @@ async def game_update(team: TeamName, world_info: World) -> list[dict[str, Comma
 
 
 @app.post("/api/statistics", status_code=status.HTTP_204_NO_CONTENT)
-async def statistics() -> None:
-    # TODO handle statistics
-    return
+async def statistics(
+    fields: Annotated[dict[str, str | int | float], Body(embed=True)]
+) -> None:
+    stat_fields = [
+        "id",
+        "team",
+        "name",
+        "gold",
+        "level",
+        "dmg_dealt_hero",
+        "dmg_dealt_struct",
+        "dmg_dealt_creep",
+        "total_dmg_dealt",
+        "dmg_received_hero",
+        "dmg_received_struct",
+        "dmg_received_creep",
+        "total_dmg_received",
+        "last_hits",
+        "kills",
+        "deaths",
+        "assists",
+        "denies",
+    ]
+    game_time = fields.pop("game_time")
+
+    stats: dict[int, dict[str, str | int | float]] = {}
+    for player_id in range(10):
+        stats[player_id] = {}
+        for stat_name in stat_fields:
+            stats[player_id][stat_name] = fields[f"{player_id}_{stat_name}"]
+
+    game_number = NUMBER_OF_GAMES - app.state.games_remaining
+    datestr = app.state.started_time.strftime("%Y%m%d_%H%M%S")
+    csv_path = Path(f"../{datestr}_statistics_{game_number}.csv")
+
+    is_new_csv = not csv_path.exists()
+    with open(csv_path, "at") as f:
+        if is_new_csv:
+            headers = ["game_time", "player_id"] + stat_fields
+            f.write(",".join(headers) + "\n")
+        for player_id in stats.keys():
+            line = [str(game_time), str(player_id)] + [
+                str(stats[player_id][k]) for k in stat_fields
+            ]
+            f.write(",".join(line) + "\n")
 
 
 @app.post("/api/restart_game", status_code=status.HTTP_204_NO_CONTENT)
 async def restart_game() -> None:
-    for team in TeamName:
-        app.state.bots[team].game_ticks = 0
-    return
+    for bot in app.state.bots.values():
+        bot.game_ticks = 0
 
 
 @app.post("/api/game_ended")
@@ -75,8 +120,8 @@ async def game_ended() -> GameStatusResponse:
     # TODO handle end statistics
     app.state.games_remaining -= 1
     if app.state.games_remaining > 0:
-        for team in TeamName:
-            app.state.bots[team].game_ticks = 0
+        for bot in app.state.bots.values():
+            bot.game_ticks = 0
         return GameStatusResponse(status="restart")
     else:
         return GameStatusResponse(status="done")
